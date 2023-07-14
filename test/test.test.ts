52b06f0aad9d4dd5bdb6617bf05252c8e78d3de0
Michał Szczepański <siematumichael@protonmail.com>
2023-07-14 15:16:55 +0200

    Testing communication using js-libp2p module but Nah to complicated for simple usecase

---
+++ test/test.test.ts
@@ -0,0 +1,46 @@
+import { test, assert, describe } from "vitest";
+import { createLibp2p } from "libp2p"
+import { webSockets } from "@libp2p/websockets";
+import { noise } from "@chainsafe/libp2p-noise";
+import { mplex } from "@libp2p/mplex";
+import { yamux } from "@chainsafe/libp2p-yamux";
+import { bootstrap } from '@libp2p/bootstrap'
+
+
+describe("peer-2-peer", () => {
+    const { log } = console;
+    
+    test("lib2p", async () => {
+        // Known peers addresses
+        const bootstrapMultiaddrs = [
+          '/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb',
+          '/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN'
+        ];
+        
+        const node = await createLibp2p({
+          transports: [webSockets()],
+          connectionEncryption: [noise()],
+          streamMuxers: [yamux(), mplex()],
+          peerDiscovery: [
+            bootstrap({
+              list: bootstrapMultiaddrs, // provide array of multiaddrs
+            })
+          ]
+        });
+        
+        node.addEventListener('peer:discovery', (evt) => {
+          console.log('Discovered %s', evt.detail.id.toString()) // Log discovered peer
+        });
+        
+        node.addEventListener('peer:connect', (evt) => {
+          console.log('Connected to %s', evt.detail.toString()) // Log connected peer
+        });
+
+        const { services } = node as any;
+        
+        services.pubsub.subscribe("news");
+        services.pubsub.addEventListener("message", (evt) => {
+            console.log("Message recived");
+        })
+    })
+})
